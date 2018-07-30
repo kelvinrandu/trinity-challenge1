d383fab4d9b193ac03862db735b8b82949df8b46
Hassan Yahya Hussein <husain.host@gmail.com>
2018-07-30 12:29:39 +0300

    [FEATURE-159394592]Add login

---
+++ user.py
@@ -0,0 +1,24 @@
+import datetime
+comment = []
+users = {}
+
+
+class User:
+    commentid = 0
+
+    def __init__(self, name, email, password, status):
+        global comment
+        global users
+        self.email = email
+        self.password = email
+        self.name = name
+        self.status = status
+
+    def create_user(self):
+        user = {self.email: {"name": self.name, "Status": self.status}}
+        users.update(user)
+        print(users)
+
+
+james = User("james", "james@example.com", "its-secret", "normal")
+james.create_user()
