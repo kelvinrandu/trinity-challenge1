25dfe1d860bada5e07f2ec8995859c8eef5c5705
Hassan Yahya Hussein <husain.host@gmail.com>
2018-07-30 14:08:33 +0300

    [FEATURE-#159394612]Add role

--- user.py
+++ user.py
@@ -36,20 +36,47 @@ class User:
         comment_data.append(my_comment)
         print(comment_data)
 
-    def edit_comment(self, commid, comment_update):
+    def edit_comment(self, commid, comment_update, status):
         self.commentid = commid
         self.comment_update = comment_update
+        self.status = status
 
         edit_this = [comm for comm in comment_data if comm['ID'] == commid]
         if len(edit_this) == 0:
             return {'message': 'No comment  found'}
         else:
-            comment_data[0]["Comment"] = comment_update
+            if status == "admin" or "moderator":
+                comment_data[0]["Comment"] = comment_update
+                entry = [
+                    entry for entry in comment_data if entry['ID'] == commid
+                ]
+                comment_data.remove(entry[0])
         print(comment_data)
 
+    def moderator_roles(self, status):
+        self.status = status
+        if status == "moderator":
+            print("You can delete any comment")
 
-james = User("james", "james@example.com", "its-secret", "normal")
+    def delete_comment(self, commitid, status):
+        self.commentid = commitid
+        edit_this = [comm for comm in comment_data if comm['ID'] == commitid]
+        if len(edit_this) == 0:
+            return {'message': 'No comment  found'}
+        else:
+            if status == "admin":
+                entry = [
+                    entry for entry in comment_data if entry['ID'] == commitid
+                ]
+                comment_data.remove(entry[0])
+                print("successfully deleted")
+            else:
+                print("You must be admin to delete")
+
+
+james = User("james", "james@example.com", "its-secret", "admin")
 james.create_user()
 james.login()
 james.comment("THis is my comment")
-james.edit_comment(0, "Edit me")
+james.edit_comment(0, "Edit me", "admin")
+james.delete_comment(0, "admin")
