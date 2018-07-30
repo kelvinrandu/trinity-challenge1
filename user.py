bc03f22bfbf3f34fc3ff46cf47c68c87394493a8
Hassan Yahya Hussein <husain.host@gmail.com>
2018-07-30 13:14:33 +0300

    [FEATURE-159394592]Add comment

--- user.py
+++ user.py
@@ -1,14 +1,14 @@
 import datetime
-comment = []
-users = {}
+comment_data = []
+users_data = {}
 
 
 class User:
     commentid = 0
 
     def __init__(self, name, email, password, status):
-        global comment
-        global users
+        global comment_data
+        global users_data
         self.email = email
         self.password = email
         self.name = name
@@ -16,14 +16,28 @@ class User:
 
     def create_user(self):
         user = {self.email: {"name": self.name, "Status": self.status}}
-        users.update(user)
-        print(users)
+        users_data.update(user)
+        print(users_data)
 
     def login(self):
         today = datetime.date.today()
         print("lastLoggedInAt ", today)
 
+    def comment(self, comment):
+        self.comment = comment
+        my_comment = {
+            "ID": User.commentid,
+            "Comment": self.comment,
+            "Created by": self.name,
+            "Created at":
+            '{:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now())
+        }
+        User.commentid += 1
+        comment_data.append(my_comment)
+        print(comment_data)
+
 
 james = User("james", "james@example.com", "its-secret", "normal")
 james.create_user()
 james.login()
+james.comment("THis is my comment")
